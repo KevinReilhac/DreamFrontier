a228ca0239f986498fa3b4f3edea8fa5ba38c3b3
Kévin Reilhac <kevin.reilhac@epitech.eu>
2020-11-14 15:07:30 +0100

    Setup test scene

---
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private Transform target = null;
+
+    private void Start()
+    {
+        if (target == null)
+            target = GameObject.FindObjectOfType<Player>()?.transform;
+    }
+
+    private void Update()
+    {
+        UpdateCameraPosition();
+    }
+
+    private void UpdateCameraPosition()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
