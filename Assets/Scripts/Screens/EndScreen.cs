228e3a40080d7a401c20fcc4a9221a148fc93a9c
Cynthia Bon <cynthia.bon@e-artsup.net>
2020-11-15 18:35:52 +0100

    Title/End screens

---
+++ Assets/Scripts/Screens/EndScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("LoadMenu", 5f);
+    }
+
+    private void LoadMenu()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
